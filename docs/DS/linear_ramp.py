a62d230d2f4e43acbfd858e8abef859bf81282a7
Travis Hoppe <travis.hoppe@gmail.com>
2019-08-27 20:37:23 -0400

    Code cleanup

--- docs/DS/linear_ramp.py
+++ docs/DS/linear_ramp.py
@@ -17,24 +17,11 @@ def compute_ramp(y, width=3):
     v = tf.sqrt(2*g*(y[0]-y))
     
     dy = y[:-1]-y[1:]
+    avg_v = (v[1:]+v[:-1])/2
+    
     h = tf.sqrt(dx**2+dy**2)
-
-    cA = 0.5*g*dy[0]/h[0]
-    t0 = tf.sqrt(4*cA*h[0])/(2*cA)
-
-    # x = x0 + v*t
-    # t = -x/v
-    t = h/((v[1:]+v[:-1])/2)
-
-    #t2 = tf.sqrt((1+dy**2/dx**2)/(2*g*(y[0]-y)[1:])) * dx
-    #print(tf.reduce_sum(t-t2))
-    #exit()
-
-    print("H[0]", h[0].numpy(), t0.numpy(), t[0].numpy() - t0.numpy())
     
-    total_time = tf.reduce_sum(t)
-    
-
+    total_time = tf.reduce_sum(h/avg_v)
     return total_time
 
 def train_step(Y):
@@ -43,26 +30,15 @@ def train_step(Y):
 
     variables = [Y]   
     grads = tape.gradient(time, variables)
-
-    #print(grads)
-    #exit()
-    
     optimizer.apply_gradients(zip(grads, variables))
-
-    # Fix the endpoints
-    #Y[0].assign(1.0)
-    #Y[-1].assign(0.0)
 
     return time.numpy()
 
 N = 100
-y = np.linspace(1.0, 0.0, N+2)[1:-1]
+y = np.linspace(1.0, 0.0, N)[1:-1]
 #y = np.random.uniform(-1,0.5,size=(N+2),)[1:-1]
 
 y = tf.Variable(y, dtype=tf.float32)
-
-
-#optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001)
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
 
 import pylab as plt
